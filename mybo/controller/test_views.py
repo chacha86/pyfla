5b96a5bac78c9067b223869faff043477c952f63
chacha86 <taejincha0619@gmail.com>
2022-04-21 18:35:20 +0900

    member func

--- mybo/controller/test_views.py
+++ mybo/controller/test_views.py
@@ -2,9 +2,9 @@ from ast import keyword
 from datetime import datetime
 from mybo import db
 from mybo.models import *
-from flask import Blueprint, render_template, request, url_for
+from flask import Blueprint, render_template, request, url_for, session, flash, g
 from werkzeug.utils import redirect
-from mybo.forms.article_forms import ArticleForm
+from mybo.forms.article_forms import ArticleForm, MemberForm, LoginForm
 
 bp = Blueprint('test', __name__, url_prefix='/test')
 
@@ -60,4 +60,84 @@ def search():
     page = request.args.get('page', type=int, default=1)
     article_list = db.session.query(Article).filter(Article.title.like('%{}%'.format(keyword)))
     article_list = article_list.paginate(page, per_page=10)
+    
     return render_template('articles/list.html', article_list=article_list)
+
+@bp.before_app_request
+def ssss() :
+    print('aaaaaaaa')
+
+@bp.before_app_request
+def load_logged_in_user() :
+    member_id = session.get('member_id')
+    if member_id is None :
+        g.member = None
+    else :
+        g.member = db.session.query(Member).get(member_id)
+
+@bp.route('login', methods=("POST", "GET"))
+def login() :
+    form = LoginForm()
+    print(form.validate_on_submit())
+    print(request.method)
+    if request.method == "POST" and form.validate_on_submit() :
+        member = get_login_member(form)
+        print(member)
+        error = None 
+        if member == None :
+            error = "잘못된 회원정보입니다."
+        else :
+            session.clear()
+            session['member_id'] = member.member_id
+            return redirect(url_for('test.list_test'))
+        flash(error)
+        
+    return render_template('members/login_form.html', form=form) 
+
+def get_login_member(form:LoginForm) :
+    login_id = form.login_id.data
+    login_pw = form.login_pw.data
+
+    member = db.session.query(Member).filter(Member.login_id == login_id and Member.login_pw == login_pw).first()
+
+    return member
+    
+@bp.route('signup', methods=("POST", "GET"))
+def signup() :
+    form = MemberForm()
+    print(request.method)
+    print(form.validate_on_submit())
+    if request.method == "POST" and form.validate_on_submit() :
+        member = add_member(form)
+        db.session.add(member)
+        db.session.commit()
+
+        return redirect(url_for('test.list_test'))
+    return render_template('members/member_form.html', form=form)    
+
+def add_member(form:MemberForm) :
+    login_id = form.login_id.data
+    login_pw = form.login_pw.data
+    nick_name = form.nick_name.data
+    real_name = form.real_name.data
+    reg_date = datetime.now()
+
+    return Member(login_id=login_id, login_pw=login_pw, nick_name=nick_name, real_name=real_name, reg_date=reg_date)
+    
+@bp.route('extends')
+def extends() :
+    
+    return render_template('test/extendsTest.html')
+
+@bp.route('include')
+def include() :
+    return render_template('test/includeTest.html')
+
+    
+result = 0
+@bp.route("accumulate/<int:num>")
+def accumulate(num) :
+    global result
+    result += num
+
+    return str(result)
--- mybo/forms/article_forms.py
+++ mybo/forms/article_forms.py
@@ -1,8 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, HiddenField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField, HiddenField, PasswordField
+from wtforms.validators import DataRequired, InputRequired, EqualTo
 
 class ArticleForm(FlaskForm) :
     title = StringField('제목', validators=[DataRequired()])
     body = TextAreaField('내용', validators=[DataRequired()])
     member_id = HiddenField('작성자', validators=[DataRequired()])
+
+class MemberForm(FlaskForm) :
+    login_id = StringField('아이디', validators=[DataRequired('아이디를 입력해주세요.')])
+    login_pw = PasswordField('비밀번호', validators=[InputRequired('비밀번호를 입력해주세요'), EqualTo('login_pw2', message='비밀번호가 일치하지 않습니다')])
+    login_pw2 = PasswordField('비밀번호2')
+    nick_name = StringField('별명', validators=[DataRequired('실명을 입력해주세요.')])
+    real_name = StringField('실명', validators=[DataRequired('별명을 입력해주세요.')])
+
+class LoginForm(FlaskForm):
+    login_id = StringField('아이디', validators=[InputRequired()])
+    login_pw = TextAreaField('비밀번호', validators=[InputRequired()])
